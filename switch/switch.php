59ffdad47e167fc902244c4b02d7d536edaf4152
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-09-01 10:19:08 +0300

    Create switch.php

---
+++ switch/switch.php
@@ -0,0 +1,10 @@
+<?php
+$favoriyemek = "Yoğurt çorbası";
+switch($favoriyemek){
+ case "Köfte":
+  echo "Favori yemeğin Köfte";
+  break;
+ case "Yoğurt çorbası":
+  echo "Favori yemeğin Yoğurt çorbası";
+  break;
+}
