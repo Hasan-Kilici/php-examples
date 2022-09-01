bd72565be431378167ed4b4ab605aeb6282a6206
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-09-01 10:10:41 +0300

    Create routers.php

---
+++ routers.php
@@ -0,0 +1,17 @@
+<?php
+
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '/' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '' :
+        require __DIR__ . '/views/index.php';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '/views/404.php';
+        break;
+}
+?>
